c3ceb7b5e94e94e4a689023d5e17374df8edfdc6
zo-ma <zero.one.mean.ability@outlook.jp>
2023-05-18 22:48:20 +0900

    feat:

--- src/pages/Components.tsx
+++ src/pages/Components.tsx
@@ -1,12 +1,54 @@
-import { Container, Heading, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Card,
+  CardBody,
+  Container,
+  HStack,
+  Heading,
+  VStack,
+} from '@chakra-ui/react'
+import { VSpacer, HSpacer } from 'Components/Spacer'
 export const Components = () => {
   return (
     <>
       <VStack>
+        <VSpacer size={4} />
         <Container maxW="container.lg">
           <Heading size="lg" textAlign="center">
             Components Mock Storybook
           </Heading>
+
+          {/*  common/Spacer/VSpacer  */}
+          <VSpacer size={8} />
+          <Heading size="lg">Spacer/VSpacer</Heading>
+          <Card variant="filled">
+            <CardBody>
+              <Box bg="green.400" w="100%" color="white">
+                Box A
+              </Box>
+              <VSpacer size={12} />
+              <Box bg="green.400" w="100%" color="white">
+                Box B
+              </Box>
+            </CardBody>
+          </Card>
+
+          {/* Spacer/HSpacer */}
+          <VSpacer size={8} />
+          <Heading size="lg">Spacer/HSpacer</Heading>
+          <Card variant="filled">
+            <CardBody>
+              <HStack>
+                <Box bg="green.400" color="white">
+                  Box A
+                </Box>
+                <HSpacer size={12} />
+                <Box bg="green.400" color="white">
+                  Box B
+                </Box>
+              </HStack>
+            </CardBody>
+          </Card>
           {/* 追加のテンプレート：消さないでね！！ */}
 
           {/* --------------------*/}
