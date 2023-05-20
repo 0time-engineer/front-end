be8120897c51394c76850c6c7bdc1cd714155dd3
kinefuchi <kinesan928@gmail.com>
2023-05-21 01:31:44 +0900

    dummydataの作成

---
+++ src/Data/DummyData.ts
@@ -0,0 +1,64 @@
+export const MemberList = [
+  {
+    address: 'abcde@gmail.com',
+    name: 'abcde',
+    src: 'https://cdn.discordapp.com/attachments/1038862997998817350/1051063658718957598/study_neko.PNG',
+  },
+  {
+    address: '1234@gmail.com',
+    name: '1234',
+    src: 'https://cdn.discordapp.com/attachments/1038862997998817350/1051063653299916820/solo_neko.PNG',
+  },
+  {
+    addres: 'aiueo@gmail.com',
+    name: 'aiueo',
+    src: 'https://cdn.discordapp.com/attachments/1038862997998817350/1051063655187353610/free_neko.PNG',
+  },
+  {
+    addres: 'qwer@gmail.com',
+    name: 'qwer',
+    src: 'https://2.bp.blogspot.com/-4cDI5-IdyKs/WUdY336_nbI/AAAAAAABFAg/xxpZDJEt6ncUrMQwU7Ug-HQA8-eRI36eACLcBGAs/s400/pet_cat_sit.png',
+  },
+]
+
+export const ScheduleWeek = [
+  {
+    start: '2023-05-21T13:00:00+09:00',
+    end: '2023-05-21T18:00:00+09:00',
+  },
+  {
+    start: '2023-05-22T13:00:00+09:00',
+    end: '2023-05-22T14:00:00+09:00',
+  },
+  {
+    start: '2023-05-23T3:00:00+09:00',
+    end: '2023-05-23T8:00:00+09:00',
+  },
+]
+
+export const ScheduleMonth = [
+  {
+    start: '2023-05-01T13:00:00+09:00',
+    end: '2023-05-01T18:00:00+09:00',
+  },
+  {
+    start: '2023-05-02T13:00:00+09:00',
+    end: '2023-05-02T14:00:00+09:00',
+  },
+  {
+    start: '2023-05-12T3:00:00+09:00',
+    end: '2023-05-12T8:00:00+09:00',
+  },
+  {
+    start: '2023-05-21T13:00:00+09:00',
+    end: '2023-05-21T18:00:00+09:00',
+  },
+  {
+    start: '2023-05-22T13:00:00+09:00',
+    end: '2023-05-22T14:00:00+09:00',
+  },
+  {
+    start: '2023-06-23T3:00:00+09:00',
+    end: '2023-06-23T8:00:00+09:00',
+  },
+]
