e580af535d6154c5e2f3819b6e64f4fde1fdb950
Soma Keisuke <zero.one.mean.ability@outlook.jp>
2023-05-22 17:33:39 +0900

    fix: bug

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import './index.css'
 
 import reportWebVitals from './reportWebVitals'
 import { App } from 'App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
 
