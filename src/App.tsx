3101bb1efade2d7793c86871ed10c382694bbab9
Soma Keisuke <zero.one.mean.ability@outlook.jp>
2023-05-23 17:10:03 +0900

    feature: page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 import { Components } from './pages/Components'
+import { BB } from 'pages/BB'
 
 export const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/bb" element={<BB />} />
           {/* コンポーネント確認用ページ */}
           <Route path="/components" element={<Components />} />
         </Routes>
---
+++ src/pages/BB.tsx
@@ -0,0 +1,12 @@
+import { NavigationBar } from 'Components/atoms/NavigationBar'
+import { VSpacer } from 'Components/atoms/Spacer'
+
+export const BB = () => {
+  return (
+    <>
+      <VSpacer size={10} />
+      <VSpacer size={32} />
+      <NavigationBar type={'Home'} />
+    </>
+  )
+}
