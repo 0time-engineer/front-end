65cb00b34b75b5e2be32abbd7e483e7a8e662224
Soma Keisuke <zero.one.mean.ability@outlook.jp>
2023-05-24 17:05:46 +0900

    feat: setvalue

--- src/Components/atoms/Gmailwindow.tsx
+++ src/Components/atoms/Gmailwindow.tsx
@@ -1,14 +1,21 @@
 import { Input, InputGroup, InputRightAddon } from '@chakra-ui/react'
+import { useState } from 'react'
 
 type Props = {
   placeholder: string
 }
 
 export const Gmailwindow = ({ placeholder }: Props) => {
+  const [text, setText] = useState('')
+
   return (
     <>
       <InputGroup size="lg">
-        <Input placeholder={placeholder} />
+        <Input
+          value={text}
+          placeholder={placeholder}
+          onChange={(event) => setText(event.target.value)}
+        />
         <InputRightAddon children="@gmail.com" bg="GrayText" />
       </InputGroup>
     </>
