c8eed4fe1b7203d1817546f45940bc30da4d6630
ksr03 <K_Haruka03@outlook.jp>
2023-05-30 00:15:59 +0900

    フレンド追加画面を作成した

--- src/pages/AddFriend.tsx
+++ src/pages/AddFriend.tsx
@@ -1,13 +1,21 @@
-import { NavigationBar } from 'Components/atoms/NavigationBar'
 import { VSpacer } from 'Components/atoms/Spacer'
+import { NavigationBar } from 'Components/atoms/NavigationBar'
+import { Gmailwindow } from 'Components/atoms/Gmailwindow'
+import { SimpleButton } from 'Components/atoms/SimpleButton'
+import { VStack } from '@chakra-ui/react'
 
 export const AddFriend = () => {
   return (
     <>
-      <VSpacer size={10} />
-      <VSpacer size={24} />
-      <VSpacer size={16} />
-      <NavigationBar type={'AddFriend'} />
+      <VStack>
+        <VSpacer size={10} />
+        <VSpacer size={24} />
+        <VSpacer size={16} />
+        <Gmailwindow placeholder={''} />
+        <VSpacer size={64} />
+        <SimpleButton type="add-friends" text="追加" isShadow={false} />
+        <NavigationBar type={'AddFriend'} />
+      </VStack>
     </>
   )
 }
