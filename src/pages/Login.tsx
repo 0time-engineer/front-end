f792640bd5667a08d719bd5d7efeafc15138ab7f
ksr03 <K_Haruka03@outlook.jp>
2023-05-31 01:22:44 +0900

    情報を取得するAPIを追加した

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,18 @@ import { Center } from '@chakra-ui/react'
 import { SimpleButton } from 'Components/atoms/SimpleButton'
 import { VSpacer } from 'Components/atoms/Spacer'
 import { TextBox } from 'Components/atoms/TextBox'
+import axios from 'axios'
+
+const getInfo = () => {
+  axios
+    .get('http://localhost:8080/login')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
 
 export const Login = () => {
   return (
@@ -12,7 +24,12 @@ export const Login = () => {
       <TextBox text={'App'} />
       <VSpacer size={64} />
       <Center>
-        <SimpleButton type={'google-icon'} text={''} isShadow={false} />
+        <SimpleButton
+          type={'google-icon'}
+          text={''}
+          isShadow={false}
+          clickFn={getInfo}
+        />
       </Center>
     </>
   )
