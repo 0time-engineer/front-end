52f6e05f2762430a84df0c758367a4e65db78190
KEiJey <keijey1015@outlook.jp>
2023-06-03 00:39:20 +0900

    feat: MyIcon作成に伴って適応を行った

--- src/Components/atoms/OneDayTimer.tsx
+++ src/Components/atoms/OneDayTimer.tsx
@@ -22,7 +22,13 @@ export const OneDayTimer = ({ oneDayList }: Props) => {
                 {clock.freeFlag ? (
                   <Box bg="#A6DFFF" w="200px" p={4}></Box>
                 ) : (
-                  <Box bg="white" w="200px" p={4}></Box>
+                  <Box
+                    bgColor="White"
+                    w="200px"
+                    p={4}
+                    borderRightColor={'black'}
+                    borderY="lg"
+                  ></Box>
                 )}
               </HStack>
             </center>
