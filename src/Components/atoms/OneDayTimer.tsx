2b190b86910937cb8bc9b734a35922745cfd70a2
KEiJey <keijey1015@outlook.jp>
2023-06-02 17:32:38 +0900

    feat: 1day用のコンポーネント作成

---
+++ src/Components/atoms/OneDayTimer.tsx
@@ -0,0 +1,34 @@
+import { Box, HStack, Text } from '@chakra-ui/react'
+
+type Props = {
+  oneDayList: {
+    hour: number
+    freeFlag: boolean
+  }[]
+}
+
+export const OneDayTimer = ({ oneDayList }: Props) => {
+  return (
+    <>
+      {oneDayList.map((clock) => {
+        return (
+          <>
+            <center>
+              <HStack>
+                <Box w="50px">
+                  <Text fontSize={20}>{clock.hour}</Text>
+                </Box>
+
+                {clock.freeFlag ? (
+                  <Box bg="#A6DFFF" w="200px" p={4}></Box>
+                ) : (
+                  <Box bg="white" w="200px" p={4}></Box>
+                )}
+              </HStack>
+            </center>
+          </>
+        )
+      })}
+    </>
+  )
+}
